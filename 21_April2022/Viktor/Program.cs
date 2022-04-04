8f3a3c504190f3d2c301b79b4909abd11866e08f
viktorstahl92 <info@viktorstahl.com>
2022-04-04 08:36:14 +0200

    Lade till Uppgift Lättare i Viktor-mappen.

---
+++ 21_April2022/Viktor/Program.cs
@@ -0,0 +1,6 @@
+﻿//Räkna antalet "D" i en mening.
+Console.WriteLine(CountD("My friend Dylan got distracted in school."));
+Console.WriteLine(CountD("Debris was scattered all over the yard."));
+Console.WriteLine(CountD("The rodents hibernated in their den."));
+
+int CountD(string sentence) => sentence.ToUpper().Count(x => x == 'D');
