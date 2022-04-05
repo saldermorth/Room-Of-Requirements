da7a1e51ac2e3d788666369af6a0ef3b73f8c568
leanderson.fredrik@gmail.com <leanderson.fredrik@gmail.com>
2022-04-05 09:45:49 +0200

    Enkla uppgiften

---
+++ 21_April2022/Fredrik/Dcounter/LetterCounter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dcounter
+{
+    public class LetterCounter
+    {
+        public char Letter { get; set; }
+
+        public LetterCounter(char letter)
+        {
+            Letter = letter;
+        }
+
+        public int CountLetters(string input) => input.ToLower().Where(x => x == Letter).Count();
+    }
+}
