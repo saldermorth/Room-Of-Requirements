cef70c6a90f1da35040ffaec32c591ec35b3d3a6
leanderson.fredrik@gmail.com <leanderson.fredrik@gmail.com>
2022-04-05 09:30:25 +0200

    Rolandz tour

---
+++ 21_April2022/Fredrik/Rolandz/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rolandz
+{
+    public class City
+    {
+        public string Name { get; } = string.Empty;
+
+        public City(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString() => Name;
+    }
+}
---
+++ 21_April2022/Fredrik/Rolandz/Trip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rolandz
+{
+    public struct Trip
+    {
+        public City[] Cities { get; }
+        public int Distance { get; }
+
+        public Trip(City cityOne, City cityTwo, int distance)
+        {
+            Cities = new City[2];
+            Cities[0] = cityOne;
+            Cities[1] = cityTwo;
+            Distance = distance;
+        }
+    }
+}
