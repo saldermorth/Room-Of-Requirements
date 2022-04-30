ce142f11de39450ff21f9763d06f46ee9f586eed
saldermorth <berg_gustav@hotmail.com>
2022-04-30 14:56:51 +0200

    Lätt och mellan klara
    Lätt och mellan klara

---
+++ 4_Maj2022/GustavsLösning/Emailchecker.cs
@@ -0,0 +1,37 @@
+﻿string email = "gustav.berg@domain.aa";
+
+bool isCorrect = false;
+char[] emailA = email.ToArray();
+int indexOne = 0;
+int indexTwo = 0;
+int indexThree = 0;
+
+
+if (email.Contains('.') && email.Contains('@'))
+{    
+    indexOne = email.LastIndexOf('.');
+    indexTwo = email.Length;
+    indexThree = email.IndexOf('@');
+    if (indexTwo - indexOne > 1)
+    {
+        
+        if (indexOne - indexThree > 2)
+        {
+            isCorrect = true;
+            Console.WriteLine("Correct email");
+        }
+        else
+        {
+            Console.WriteLine("Email need a minimum of three characters between @ and last .");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Email need a minimum of two characters after last . ");
+    }
+
+}
+else
+{
+    Console.WriteLine("Needs both . and @ in email");
+}
---
+++ 4_Maj2022/GustavsLösning/NameSwitch.cs
@@ -0,0 +1,15 @@
+﻿string name = "Gustav Berg";
+
+string[] namereverce = name.Split(' ');
+
+name = "";
+
+name = namereverce[1] + " ";
+
+
+name += namereverce[0];
+
+
+Console.WriteLine(name);
+
+Console.ReadLine();
