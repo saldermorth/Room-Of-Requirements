1f27522c6b664468a3d559abc49b5b18d5f3c856
saldermorth <berg_gustav@hotmail.com>
2022-05-15 16:07:17 +0200

    lättare done

---
+++ 25_Maj2022/Gustav/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace problemlösning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            lättare lätt = new lättare();
+            lätt.Findx();
+        }
+    }
+}
---
+++ 25_Maj2022/Gustav/lättare.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problemlösning
+{
+    internal class lättare
+    {
+
+        /*Problem Lättare:​
+You are given an array of positive numbers from 1 to n, such that all numbers from 1 to n are present except one number 'x'. We have to find 'x'. The input array is not sorted.*/
+
+        public void Findx()
+        {
+            int[] numbers = { 12, 5, 6, 3, 9, 7, 8, 10, 1, 4, 2 };
+            ////////////////{  1, 2, 3, 4, 5, 6, 7, 8, 9 ,10
+            int[] result = new int[numbers.Length];
+            int notfound = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result[i] = i + 1;
+            }
+
+            foreach (var item in numbers) //faulty array
+            {
+                int index = numbers.Length;
+                foreach (var item2 in result)//correct faulty array
+                {
+                    index--;
+                    if (item == item2)
+                    {
+                        break;
+                    }
+                    else if (index < 1 && item != item2)
+                    {
+                        Console.WriteLine(item2 + " not found");
+                        Console.ReadLine();
+                    }
+                }
+            }
+
+        }
+
+    }
+}
